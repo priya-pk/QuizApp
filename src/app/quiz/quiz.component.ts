8cba2e724300d509f42484c6b0240115cf3b8cf9
Priya Jose <priyajoz96@gmail.com>
2020-09-22 14:51:18 +0530

    3rd commit

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -9,19 +9,32 @@ import { QuestionsService } from '../questions.service';
 })
 export class QuizComponent implements OnInit {
 
-  questions:Question[]
-	isAnswerShow: boolean = false;
+  questions:Question[];
+  isAnswerShow: boolean = false;
+  seconds:number;
+  timer;
+  qnProgress:number;
 
   constructor(private _quest: QuestionsService) { }
 
   ngOnInit(): void {
+    this.seconds = 0;
+    this.qnProgress = 0;
+    
     this._quest.getQuest()
     .subscribe((data)=>{
       this.questions=JSON.parse(JSON.stringify(data));
+      this.startTimer();
     })
   }
   // showAnswer(i){
   //   this.isAnswerShow =!this.isAnswerShow;
   // }
+
+  startTimer() {
+    this.timer = setInterval(()=>{
+      this.seconds++;
+    },1000);
+  }
 
 }
